c6bebb8ecb7a4a9f467455084292fb78441f730a
Dan Winship <danw@src.gnome.org>
2008-09-04 12:44:21 +0000

    create python bindings for GWeatherLocation, GWeatherTimezone,
    	* python/*: create python bindings for GWeatherLocation,
    	GWeatherTimezone, GWeatherLocationEntry, and GWeatherTimezoneMenu.
    	As with the C library, these are unstable. #547824
    
    	* configure.in: Add --enable-python (off by default)
    
    	* acinclude.m4: stuff needed for that
    
    svn path=/trunk/; revision=376

---
+++ python/bad__init__.py
@@ -0,0 +1,11 @@
+# The correct idiom is
+# from gweather.I_KNOW_THIS_IS_UNSTABLE import gweather
+
+def location_new_world(use_regions):
+    raise ImportError("libgweather should only be used if you understand that it's subject to change, and is not supported as a fixed API/ABI or as part of the platform")
+
+def location_entry_new(top):
+    raise ImportError("libgweather should only be used if you understand that it's subject to change, and is not supported as a fixed API/ABI or as part of the platform")
+
+def timezone_menu_new(top):
+    raise ImportError("libgweather should only be used if you understand that it's subject to change, and is not supported as a fixed API/ABI or as part of the platform")
