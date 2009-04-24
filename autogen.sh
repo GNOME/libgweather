408b97379ee98d7f8db890a0f1ee7b98184d80c3
Vincent Untz <vuntz@gnome.org>
2009-04-24 03:25:22 +0200

    Make autogen.sh more modern.

--- autogen.sh
+++ autogen.sh
@@ -5,19 +5,18 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="libgweather"
+REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
   && test -d $srcdir/libgweather) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level libgweather directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
-
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common."
     exit 1
 }
-REQUIRED_AUTOMAKE_VERSION=1.9 \
-USE_GNOME2_MACROS=1 \
+
 . gnome-autogen.sh
