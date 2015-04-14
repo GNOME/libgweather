601beca242e871391c47fb6234b7bafc93a6c4c2
Giovanni Campagna <gcampagna@src.gnome.org>
2015-04-13 23:03:55 -0700

    Add dummy C files corresponding to header files
    All header files must have a C file with the same name (minus
    the extension), for gnome-builder to pick up the correct C
    flags. So add mostly empty files that include the right header.

---
+++ libgweather/gweather-private.c
@@ -0,0 +1,29 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* weather.c - Overall weather server functions
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "gweather-private.h"
+
+/*
+  There is no code here: the purpose of this header is so that
+  gnome-builder knows how to pick up the include flags to parse
+  gweather-private.h properly.
+*/
