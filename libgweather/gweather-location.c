GNOME/libgweather

commit 96d1ac23977d422a5e2eb1f42b51f2e20793348f

GWeatherLocation: Add private function to reset the world
So that we can test easily the different envvars.

libgweather/gweather-location.c
@@ -377,7 +377,15 @@ error_out:
     return NULL;
 }
 
-static GWeatherLocation *global_world;
+static GWeatherLocation *global_world = NULL;
+
+static void _gweather_location_unref_no_check (GWeatherLocation *loc);
+
+GWEATHER_EXTERN void
+_gweather_location_reset_world (void)
+{
+	g_clear_pointer (&global_world, _gweather_location_unref_no_check);
+}
 
 /**
  * gweather_location_get_world:
@@ -438,24 +446,13 @@ gweather_location_ref (GWeatherLocation *loc)
     return loc;
 }
 
-/**
- * gweather_location_unref:
- * @loc: a #GWeatherLocation
- *
- * Subtracts 1 from @loc's reference count, and frees it if the
- * reference count reaches 0.
- **/
-void
-gweather_location_unref (GWeatherLocation *loc)
+static void
+_gweather_location_unref_no_check (GWeatherLocation *loc)
 {
     int i;
 
-    g_return_if_fail (loc != NULL);
-
     if (--loc->ref_count)
 	return;
-
-    g_return_if_fail (loc->level != GWEATHER_LOCATION_WORLD);
 
     g_free (loc->english_name);
     g_free (loc->local_name);
@@ -490,6 +487,22 @@ gweather_location_unref (GWeatherLocation *loc)
 	g_hash_table_unref (loc->country_code_cache);
 
     g_slice_free (GWeatherLocation, loc);
+}
+
+/**
+ * gweather_location_unref:
+ * @loc: a #GWeatherLocation
+ *
+ * Subtracts 1 from @loc's reference count, and frees it if the
+ * reference count reaches 0.
+ **/
+void
+gweather_location_unref (GWeatherLocation *loc)
+{
+    g_return_if_fail (loc != NULL);
+    g_return_if_fail (loc->level != GWEATHER_LOCATION_WORLD);
+
+    _gweather_location_unref_no_check (loc);
 }
 
 GType

libgweather/test_libgweather.c
@@ -27,6 +27,8 @@
 #include "gweather-location.h"
 #include "gweather-weather.h"
 
+extern void _gweather_location_reset_world (void);
+
 /* For test_metar_weather_stations */
 #define METAR_SOURCES "https://www.aviationweather.gov/docs/metar/stations.txt"
 

commit c50aad90d572e3f2300b5f35b89b8085d0e88679

GWeatherLocation: Fix warning on entry exit
The world is not supposed to be unref'ed "by hand", but any extra
references can be dropped. This fixes a warning on exit when destroying
a location entry.

 #0  0x00007ffff775d295 in _g_log_abort (breakpoint=1) at glib/glib/gmessages.c:554
 #1  0x00007ffff775e4a9 in g_logv (log_domain=0x7ffff7fc1cc1 "GWeather", log_level=G_LOG_LEVEL_CRITICAL, format=<optimized out>, args=args@entry=0x7fffffffc988)
    at glib/glib/gmessages.c:1371
 #2  0x00007ffff775e662 in g_log (log_domain=<optimized out>, log_level=<optimized out>, format=<optimized out>) at glib/glib/gmessages.c:1413
 #3  0x00007ffff7fb765d in gweather_location_unref (loc=0x642c90) at libgweather/libgweather/gweather-location.c:548
 #4  0x00007ffff7fba914 in finalize (object=0x56d230) at libgweather/libgweather/gweather-location-entry.c:149

Fixes: 96d1ac2

libgweather/gweather-location.c
@@ -545,7 +545,7 @@ void
 gweather_location_unref (GWeatherLocation *loc)
 {
     g_return_if_fail (loc != NULL);
-    g_return_if_fail (loc->level != GWEATHER_LOCATION_WORLD);
+    g_return_if_fail (loc->level != GWEATHER_LOCATION_WORLD || loc->ref_count > 1);
 
     _gweather_location_unref_no_check (loc);
 }