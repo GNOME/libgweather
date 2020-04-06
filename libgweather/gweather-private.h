1d423844b7fdd74e11f04d801779018dd3ec5a9b
Bastien Nocera <hadess@hadess.net>
2020-04-06 13:14:57 +0200

    all: Use https for GNU license link

--- libgweather/gweather-private.h
+++ libgweather/gweather-private.h
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see
- * <http://www.gnu.org/licenses/>.
+ * <https://www.gnu.org/licenses/>.
  */
 
 #ifndef __WEATHER_PRIV_H_
