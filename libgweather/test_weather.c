6e46db89d46178615ae17e5b6f560677e8679574
Bastien Nocera <hadess@hadess.net>
2021-01-12 17:53:04 +0100

    tests: Fix weather test not setting contact info

--- libgweather/test_weather.c
+++ libgweather/test_weather.c
@@ -156,6 +156,7 @@ main (int argc, char **argv)
 
     loop = g_main_loop_new (NULL, TRUE);
     info = gweather_info_new (NULL);
+    gweather_info_set_contact_info (info, "https://gitlab.gnome.org/GNOME/libgweather/");
     gweather_info_set_application_id (info, "org.gnome.LibGWeather");
     if (!set_providers (info))
         return 1;
