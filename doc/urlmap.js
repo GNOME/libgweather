d9c3da480af59a51fec94524d21991f8fee39bb9
Emmanuele Bassi <ebassi@gnome.org>
2021-10-12 12:30:40 +0100

    Use gi-docgen to generate the API reference
    Gtk-doc is currently unmaintained, and fairly slow.
    
    Gi-docgen uses the introspection data to generate the documentation.

---
+++ doc/urlmap.js
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2021 Emmanuele Bassi
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk3/' ],
+    [ 'GdkPixbuf', 'https://docs.gtk.org/gdk-pixbuf/' ],
+]
